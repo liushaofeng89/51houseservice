a5f5a2d8537c67fe8b4e29644e27ef489c57ae0b
liushaofeng <liushaofeng89@qq.com>
2015-09-26 15:29:08 +0800

    添加login页面

---
+++ WebContent/resource/javascripts/com.51houseservice.login.js
@@ -0,0 +1,11 @@
+$(function() { 
+	    $("button[type='submit']").click(function(){
+	       $(this).button('loading').delay(1000).queue(function() {
+	          $(this).button('loading');
+	       });        
+	    });
+	    
+	    //动画效果（http://www.cnblogs.com/2050/p/3409129.html）
+	    $("#logo").addClass("animated bounceInDown");
+	    $(".login-content").addClass("animated flipInX");
+	}); 
